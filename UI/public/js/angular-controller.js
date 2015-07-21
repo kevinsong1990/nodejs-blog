58ddeeceef4ff213b09516f4af745a1708e6ee7b
kevinsong <songkailun@foxmail.com>
2015-07-21 14:19:59 +0800

    update html, js

---
+++ UI/public/js/angular-controller.js
@@ -0,0 +1,7 @@
+// Declare angular JS level module wich depends on filters, and services
+var myControllers = angular.module('myControllers', []);
+
+// controller
+myControllers.controller('indexContrl', ['$scope', function ($scope) {
+    //$scope.name = "Kevin";
+}]);
