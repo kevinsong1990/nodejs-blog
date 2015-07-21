518cf7d28bc0712ce510c5af6c5fa3e6aca4349b
kevinsong <songkailun@foxmail.com>
2015-07-21 14:21:33 +0800

    delete useless file

--- UI/public/js/controller.js
+++
@@ -1,7 +0,0 @@
-// Declare angular JS level module wich depends on filters, and services
-var myControllers = angular.module('myControllers', []);
-
-// controller
-myControllers.controller('indexContrl', ['$scope', function ($scope) {
-    //$scope.name = "Kevin";
-}]);
