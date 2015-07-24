42ccc0d4af01a56eb312d2dbb4cd0061e9a27c92
kevinsong <songkailun@foxmail.com>
2015-07-24 19:36:17 +0800

    add facotry, add http call

--- UI/public/_pages/index/controller.js
+++ UI/public/_pages/index/controller.js
@@ -1,4 +1,22 @@
 // controller
-myControllers.controller('indexContrl', ['$scope', function ($scope) {
-    $scope.name = "Kevin";
+myControllers.controller('indexContrl', ['$scope', 'RestServer', function ($scope, RestServer) {
+    // get article list from server
+    var get_article_list = function () {
+        RestServer.get(
+            "/get_article_list",
+            function (response) {
+                // success
+                console.log(response);
+            },
+            function (response) {
+                // failed
+                console.log("Error when get article list.");
+                $scope.errorMessage = err;
+            }
+        );
+    };
+
+    // main
+    console.log("get article list");
+    get_article_list();
 }]);
