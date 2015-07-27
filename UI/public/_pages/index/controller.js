5a4d2689f535833b697f65b3568e233be321e11c
kevinsong <songkailun@foxmail.com>
2015-07-27 13:30:39 +0800

    add paging - show next or previous button

--- UI/public/_pages/index/controller.js
+++ UI/public/_pages/index/controller.js
@@ -4,18 +4,43 @@ myControllers.controller('indexContrl', ['$scope', 'RestServer', function ($scop
     $scope.article_list = [];
     // default show 5 articles per page
     $scope.article_num_per_page = 5;
-    // current article list
-    $scope.current_article_num  = 0;
+    // current article list, default is the 1st page
+    $scope.current_page = 1;
     // article number we got from backend
     $scope.total_aritcle_num = 0;
-        
+    // show the "before" button
+    $scope.show_before = false;
+    // show the "next" button
+    $scope.show_next = false;
+    
+
+    // show or hide the before/next button for paging
+    var controllButtonShowOrHide = function () {
+        //console.log("$scope.total_aritcle_num: " + $scope.total_aritcle_num + ", $scope.article_num_per_page: " + $scope.article_num_per_page + ", $scope.current_page: " + $scope.current_page);
+        if ($scope.total_aritcle_num > $scope.article_num_per_page * $scope.current_page) {
+            // it means there are still some articles didn't show
+            $scope.show_next = true;
+        }
+        else {
+            $scope.show_next = false;
+        }
+
+        if ($scope.current_page > 1) {
+            // it means we are not the first page now
+            $scope.show_before = true;
+        }
+        else {
+            $scope.show_before = false;
+        }
+    };
+    
     // get article list from server
-    var get_article_list = function (current_article_num, article_num_per_page) {
+    var get_article_list = function (current_page, article_num_per_page) {
         RestServer.post(
             "/get_article_list",
             {
-                article_begin: current_article_num,
-                article_end: current_article_num + article_num_per_page
+                article_begin: (current_page - 1) * article_num_per_page,
+                article_end: current_page * article_num_per_page
             },
             function (response) {
                 if (response.result === "success") {
@@ -24,6 +49,9 @@ myControllers.controller('indexContrl', ['$scope', 'RestServer', function ($scop
                     
                     $scope.total_aritcle_num = response.data.total_aritcle_num;
                     console.log("article number: " + $scope.total_aritcle_num);
+
+                    // init the article_list
+                    $scope.article_list = [];
 
                     var list = response.data.article_list;
                     for (var i=0; i<list.length; i++) {
@@ -46,6 +74,10 @@ myControllers.controller('indexContrl', ['$scope', 'RestServer', function ($scop
                     console.log("get_article_list fail, error: " + response.error);
                     $scope.errorMessage = response.error;
                 }
+
+                // show button
+                controllButtonShowOrHide();
+                //console.log("$scope.show_before: " + $scope.show_before + ", $scope.show_next: " + $scope.show_next);
             },
             function (err) {
                 // failed
@@ -54,7 +86,22 @@ myControllers.controller('indexContrl', ['$scope', 'RestServer', function ($scop
             }
         );
     };
-    
+
+    $scope.showBefore = function () {
+        if ($scope.current_page <= 1) {
+            $scope.errorMessage = "This is already the 1st page.";
+            return;
+        }
+
+        $scope.current_page = $scope.current_page - 1;
+        get_article_list($scope.current_page, $scope.article_num_per_page); 
+    };
+
+    $scope.showNext = function () {
+        $scope.current_page = $scope.current_page + 1;
+        get_article_list($scope.current_page, $scope.article_num_per_page);
+    };
+
     // main
-    get_article_list($scope.current_article_num, $scope.article_num_per_page);
+    get_article_list($scope.current_page, $scope.article_num_per_page);
 }]);
