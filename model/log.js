9ca3770d42426ab48c304fd04ac6043d560419aa
kevinsong <songkailun@foxmail.com>
2015-08-14 15:24:11 +0800

    use different path in windows and linux

--- model/log.js
+++ model/log.js
@@ -1,18 +1,38 @@
 var winston = require('winston');
+var os      = require('os');
 var config  = require('./../config.json');
 
+// we need to choose different log path for different platform
+var platform = os.platform();
+
 // log config
-var log = new (winston.Logger)({
-	transports : [
-	new (winston.transports.File)({
-		filename : config.log.filename,
-		dirname  : config.log.path,
-		maxsize  : config.log.maxsize,
-		maxFiles : config.log.maxFiles,
-        level    : config.log.level,
-		json : false
-	})]
-});
+var log;
+if (platform.indexOf("win") != -1) {
+    log = new (winston.Logger)({
+	    transports : [
+	    new (winston.transports.File)({
+		    filename : config.log.filename,
+		    dirname  : config.log.path.windows,
+		    maxsize  : config.log.maxsize,
+		    maxFiles : config.log.maxFiles,
+            level    : config.log.level,
+		    json : false
+	    })]
+    });
+}
+else {
+    log = new (winston.Logger)({
+	    transports : [
+	    new (winston.transports.File)({
+		    filename : config.log.filename,
+		    dirname  : config.log.path.linux,
+		    maxsize  : config.log.maxsize,
+		    maxFiles : config.log.maxFiles,
+            level    : config.log.level,
+		    json : false
+	    })]
+    });
+}
 
 // export
 module.exports = log;
